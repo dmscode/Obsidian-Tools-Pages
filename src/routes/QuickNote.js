635b6d636af89b0b0d325465aed86011066c38b7
dmscode <i@zji.me>
2022-12-20 10:16:31 +0800

    Add Clear all button

--- src/routes/QuickNote.js
+++ src/routes/QuickNote.js
@@ -1,6 +1,16 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear, faNoteSticky, faPaperPlane, faMugHot, faBroom } from '@fortawesome/free-solid-svg-icons'
 import './QuickNote.css';
 
+/* ======== 按钮图标 ======== */
+const configIcon = <FontAwesomeIcon icon={faGear} />
+const noteIcon = <FontAwesomeIcon icon={faNoteSticky} />
+const sendIcon = <FontAwesomeIcon icon={faPaperPlane} />
+const coffeeIcon = <FontAwesomeIcon icon={faMugHot} />
+const broomIcon = <FontAwesomeIcon icon={faBroom} />
+
+/* ======== 默认设置 ======== */
 const defaultPath = 'obsidian://advanced-uri?filepath=Logs%252F{{date}}.md'
 const defaultTemplate = `---
 created: {{date}} {{time}}
@@ -17,6 +27,7 @@ updated: {{date}} {{time}}
 const dbNum = num => String(num).padStart(2, '0')
 
 const QuickNote = () => {
+  const contentEl = useRef(null)
   const [showConfig, setShowConfig] = useState(()=>!window.localStorage.getItem('QuickNotePath') || !window.localStorage.getItem('QuickNoteTemplate'))
   const [notePath, setNotePath] = useState(()=>window.localStorage.getItem('QuickNotePath') || defaultPath)
   const [template, setTemplate] = useState(()=>window.localStorage.getItem('QuickNoteTemplate') || defaultTemplate)
@@ -25,6 +36,10 @@ const QuickNote = () => {
   useEffect(()=>{ window.localStorage.setItem('QuickNotePath', notePath) }, [notePath])
   useEffect(()=>{ window.localStorage.setItem('QuickNoteTemplate', template) }, [template])
   useEffect(()=>{ window.localStorage.setItem('QuickNoteContent', note) }, [note])
+
+  useEffect(()=>{
+    if(!showConfig) contentEl.current.focus()
+  }, [showConfig])
 
   const sendNote = useCallback(()=>{
     const now = new Date()
@@ -44,42 +59,65 @@ const QuickNote = () => {
     const data = template.replace(/\{\{(\w+)\}\}/g, (m, s)=> keywords[s] || m)
     window.open(`${path}&data=${encodeURIComponent(data)}&mode=append`)
   }, [note, notePath, template])
+  const updatePath = useCallback((e)=>{
+    const newPath = decodeURIComponent(decodeURIComponent(e.target.value.replace(/\n/, '')))
+    if(newPath === notePath) return
+    setNotePath(newPath)
+  }, [notePath, setNotePath])
+  const clearAll = useCallback(()=>{
+    if(window.confirm('Clear all content ?')) setNote('')
+  }, [])
   return (
     <div className="quick-note">
       {
         showConfig
         ? (
           <div className="quick-note-config">
+            <div className="card">
             <label htmlFor="quick-note-path">笔记地址（Advanced URI 格式）：</label>
-            <input
+            <textarea
               id="quick-note-path"
               placeholder="笔记地址（Advanced URI 格式），可以使用下方变量。"
-              value={notePath || ''}
-              onChange={e=>setNotePath(e.target.value)}></input>
+              value={notePath.replace(/(\?|&)/g, '\n$1') || ''}
+              onChange={updatePath}></textarea>
+            </div>
+            <div className="card">
             <label htmlFor="quick-note-template">笔记模板：</label>
             <textarea
               id="quick-note-template"
               placeholder="笔记模板，可以使用下方变量。考虑到可能追加在原有笔记中，所以不建议比较复杂。"
               value={template || ''}
               onChange={e=>setTemplate(e.target.value)}></textarea>
+            </div>
           </div>
         )
-        : <textarea
-            id="quick-note-content"
-            placeholder="写下笔记，然后发送……"
-            value={note || ''}
-            onChange={e=>setNote(e.target.value)}></textarea>
+        : <div className="card quick-note-input">
+            <textarea
+              id="quick-note-content"
+              ref={contentEl}
+              placeholder="写下笔记，然后发送……"
+              value={note || ''}
+              onChange={e=>setNote(e.target.value)}></textarea>
+            {(note&&note.length) && <button className="clear-all" onClick={clearAll}>{broomIcon}</button>}
+          </div>
       }
-      <button className="config" onClick={()=>{setShowConfig(nowState=>!nowState)}}>{showConfig ? 'Note' : 'Config'}</button>
-      {!showConfig && <button className="Save" onClick={sendNote}>Send Note</button>}
+      <div className="card button-group">
+        <button className="toggle" onClick={()=>{setShowConfig(nowState=>!nowState)}}> {showConfig ? noteIcon : configIcon } </button>
+        {
+          showConfig
+          ? <button className="coffee" onClick={()=>{window.open('https://afdian.net/a/daomishu', '_blank')}}>{coffeeIcon} Coffee</button>
+          : <button className="send" onClick={sendNote}>Send Note {sendIcon}</button>}
+      </div>
+      
       {showConfig && (
-        <div className="quick-note-desc">
+        <div className="card quick-note-desc">
           <h3>使用说明：</h3>
           <p>本工具基于 <a href="https://github.com/Vinzent03/obsidian-advanced-uri">Advanced URI</a> 的 Api，故需要先安装此插件。</p>
           <p>然后在你预定的位置创建一篇笔记，复制这篇笔记的 Advanced URI ，粘贴到本工具的笔记地址栏。这样所有新添加内容都会追加到这篇笔记中。</p>
           <p>你也可以在地址中使用变量，变量的格式：<code>&#123;&#123;关键字&#125;&#125;</code>。从而实现按一定规则自动创建笔记。</p>
           <p>模板中同样可以使用变量实现 Front matter 的添加。需要注意：<strong>模板中必须用 <code>&#123;&#123;content&#125;&#125;</code> 变量来插入笔记的内容</strong>，否则就不包含你书写的笔记了哦~</p>
           <p>工具有数据自动保存和自动恢复，只是以防意外，必要过度依赖，不要使用浏览器的退出自动清除浏览数据或者隐私浏览模式。</p>
+          <p>输入内容后输入框右上角有一个半透明按钮（不太明显），点击可以清空输入框。</p>
           <p><strong>可用变量关键字：</strong></p>
           <ul>
           <li><code>content</code>：你输入的笔记内容</li>
